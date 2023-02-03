38f83e322dbaeed3367b99c722d1fb64cee59e93
Raphael COUTAUD <raphael.coutaud.cap@gmail.com>
2023-02-03 12:22:42 +0100

    ajout de redux et du store

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,12 @@ import React from "react";
 import { BrowserRouter as Router , Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  const user = JSON.parse(localStorage.getItem('profile'));
-
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<div></div>}/>
-        <Route exact path="/profile/" element={<div></div>}/>        
-        {!user ? (
-          <Route exact path="/auth" element={<div></div>}/>
-        ) : (
-          <Route exact path="/auth" element={<Navigate to="/" />}/>
-        )}        
+        <Route exact path="/profile" element={<div></div>}/>        
+        <Route exact path="/auth" element={<Navigate to="/" />}/>
       </Routes>
     </Router>
   );
