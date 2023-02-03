af4670610b1d36c85ee679d635ba3e00cf4a2e73
Raphael COUTAUD <raphael.coutaud.cap@gmail.com>
2023-02-03 14:19:46 +0100

    initialisation du node serveur

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+dotenv.config();
+
+//middleware
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
