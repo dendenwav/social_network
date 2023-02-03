18e575cbc02e4effb2fb27215b6d85bf245d096c
Raphael COUTAUD <raphael.coutaud.cap@gmail.com>
2023-02-03 16:28:09 +0100

    debut ajout authentification

---
+++ server/config/keys.js
@@ -0,0 +1,3 @@
+const secret = 'mysecretkey';
+
+export default secret;
--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
+import passport from './config/passport.js';
+
 const app = express();
 dotenv.config();
 
