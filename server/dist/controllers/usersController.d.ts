a5898264965a5771c561e985fc3c1d90607a9ad2
Raphael COUTAUD <raphael.coutaud.cap@gmail.com>
2023-06-30 16:14:19 +0200

    regen du dist folder

--- server/dist/controllers/usersController.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-export declare const getUsers: (req: Request, res: Response) => Promise<void>;
-export declare const getUser: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const updateUser: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const deleteUser: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const followUser: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-export declare const unfollowUser: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -4,7 +4,7 @@ import { IUser } from "../models/_interfaces/UserInterfaces";
 declare global {
     namespace Express {
         interface Request {
-            user?: IUser
+            user?: IUser;
         }
     }
 }
